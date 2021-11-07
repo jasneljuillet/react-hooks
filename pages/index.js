9990078e45e998fd38f047a5560c23cc3dd6c30a
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-11-07 15:31:08 -0500

    Make some changes

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,13 @@ export default function Home() {
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://jasnel.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          {'Copywrite'}
+          <img src="/file.enc.jpg" alt="Jasnel Photo" className="h-4 ml-2 rounded-full" />
+          {' Jasnel'}
         </a>
       </footer>
     </div>
